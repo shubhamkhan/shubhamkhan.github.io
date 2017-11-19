7b54faa2969e945865a51be9fd473ededc7df448
Shubham Khan <shubhamkhanit@gmail.com>
2017-11-19 23:47:53 +0530

    Version 0.6 compress image

--- js/custom.js
+++ js/custom.js
@@ -32,33 +32,9 @@ $(document).ready(function(){
         } else {
             $('.navbar').removeClass('fixed-top nav-shadow');
         }
-        if ($(this).scrollTop() > 3800) {
-          $('.progress .progress-bar').css("width",function(){
-               return $(this).attr("aria-valuenow") + "%";
-           });
-        }
     });
     $('#scroll').click(function(){
         $("html, body").animate({ scrollTop: 0 }, 600);
         return false;
     });
 });
-
-// $(document).ready(function() {
-//      $('.progress .progress-bar').css("width",function(){
-//           return $(this).attr("aria-valuenow") + "%";
-//       });
-//    });
-
-
-
-
-//Facebook Follow, Like and Share code start
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10&appId=273857719778432";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-//Facebook Follow, Like and Share code end
