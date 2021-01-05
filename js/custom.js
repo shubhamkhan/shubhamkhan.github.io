edadcdda5556fe2927fd2c7c6e135a947470465c
Shubham Khan <shubhamkhanit@gmail.com>
2021-01-05 22:05:50 +0530

    Var 2.0

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,63 @@
+//Progress bar effect for Skill
+$(function() {
+  var current_progress = 0;
+  var interval = setInterval(function() {
+      current_progress += 1;
+      $("#dynamic")
+      .css("width", current_progress + "%" )
+      .attr("aria-valuenow", current_progress)
+      .text("C Programming " + current_progress + "%");
+      if (current_progress >= 75)
+          clearInterval(interval);
+  }, 150);
+  var interval1 = setInterval(function() {
+      current_progress += 1;
+      $("#dynamic1")
+      .css("width", current_progress + "%" )
+      .attr("aria-valuenow", current_progress)
+      .text("PHP " + current_progress + "%");
+      if (current_progress >= 65)
+          clearInterval(interval1);
+  }, 150);
+  var interval2 = setInterval(function() {
+      current_progress += 1;
+      $("#dynamic2")
+      .css("width", current_progress + "%" )
+      .attr("aria-valuenow", current_progress)
+      .text("Python " + current_progress + "%");
+      if (current_progress >= 55)
+          clearInterval(interval2);
+  }, 150);
+  var interval3 = setInterval(function() {
+      current_progress += 1;
+      $("#dynamic3")
+      .css("width", current_progress + "%" )
+      .attr("aria-valuenow", current_progress)
+      .text("HTML & CSS " + current_progress + "%");
+      if (current_progress >= 80)
+          clearInterval(interval3);
+  }, 150);
+  var interval4 = setInterval(function() {
+      current_progress += 1;
+      $("#dynamic4")
+      .css("width", current_progress + "%" )
+      .attr("aria-valuenow", current_progress)
+      .text("Bootstrap " + current_progress + "%");
+      if (current_progress >= 85)
+          clearInterval(interval4);
+  }, 150);
+  var interval5 = setInterval(function() {
+      current_progress += 1;
+      $("#dynamic5")
+      .css("width", current_progress + "%" )
+      .attr("aria-valuenow", current_progress)
+      .text("MySQL " + current_progress + "%");
+      if (current_progress >= 60)
+          clearInterval(interval5);
+  }, 150);
+});
+
+
 //Scroll effect for navbar
 $(document).ready(function(){
   $('body').scrollspy({target: ".navbar", offset: 0});
@@ -7,26 +67,29 @@ $(document).ready(function(){
       var hash = this.hash;
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 800, function(){
+      }, 900, function(){
         window.location.hash = hash;
       });
     }
   });
 });
 
-//Scroll effect for Navbar shadow, Mouse Downs and Top to Buttom button
+//Scroll effect for Navbar shadow, Mouse Downs and Buttom to Top button
 $(document).ready(function(){
     $(window).scroll(function(){
-        if ($(this).scrollTop() > 90) {
-            $('.scroll-downs').fadeOut();
-        } else {
-            $('.scroll-downs').fadeIn();
-        }
+       // Mouse Downs
         if ($(this).scrollTop() > 100) {
             $('#scroll').fadeIn();
         } else {
             $('#scroll').fadeOut();
         }
+        // Buttom to Top button
+        if ($(this).scrollTop() > 90) {
+            $('.scroll-downs').fadeOut();
+        } else {
+            $('.scroll-downs').fadeIn();
+        }
+        // Navbar shadow
         if ($(this).scrollTop() > 675) {
             $('.navbar').addClass('fixed-top nav-shadow');
         } else {
